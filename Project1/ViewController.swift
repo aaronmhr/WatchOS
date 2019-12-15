af686232cc451ca15739bf617edd7a17423a57a3
aaronmhr <aaronmhr@gmail.com>
2019-12-15 22:55:34 +0100

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Aaron Huánuco on 15/12/2019.
+//  Copyright © 2019 Aaron Huánuco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
