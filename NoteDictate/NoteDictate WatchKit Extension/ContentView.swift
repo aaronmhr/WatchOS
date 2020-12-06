24a3968dd5bc1d2cd6c6a45a08854d762ae59523
aaronmhr <aaronmhr@gmail.com>
2020-12-06 21:34:22 +0100

    note-dictate – Add new NodeDictate project

---
+++ NoteDictate/NoteDictate WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NoteDictate WatchKit Extension
+//
+//  Created by Aaron Huánuco on 6/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NoteDictate/NoteDictate WatchKit Extension/NoteDictateApp.swift
@@ -0,0 +1,19 @@
+//
+//  NoteDictateApp.swift
+//  NoteDictate WatchKit Extension
+//
+//  Created by Aaron Huánuco on 6/12/20.
+//
+
+import SwiftUI
+
+@main
+struct NoteDictateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
